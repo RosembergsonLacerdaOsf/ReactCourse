2136f2e97793ef0c61ebc8980c15d929814570e3
RosembergsonLacerdaOsf <rosembergson.lacerda@osf-commerce.com>
2019-11-01 13:41:24 -0300

    code

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import First from './Component/firstComponent'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <First />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react-complete-guide/src/Component/firstComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const firstComponent = () => {
+    return <p>I'm the firstComponent</p>
+}
+
+export default firstComponent;
